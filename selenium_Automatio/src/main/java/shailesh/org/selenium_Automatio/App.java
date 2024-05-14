6f60584397ff9a7661b7e19b464b2505af3cc7b5
shubham <maneshubham599@gmail.com>
2024-05-14 18:54:45 +0530

    Added standard structure

---
+++ selenium_Automatio/src/main/java/shailesh/org/selenium_Automatio/App.java
@@ -0,0 +1,13 @@
+package shailesh.org.selenium_Automatio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
