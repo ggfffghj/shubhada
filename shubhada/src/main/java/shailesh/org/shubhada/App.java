a27e0f96d550cf6e2e0ba7e7c5b6842cb6cc23d5
shubham <maneshubham599@gmail.com>
2024-05-14 17:15:32 +0530

    commit project structure

---
+++ shubhada/src/main/java/shailesh/org/shubhada/App.java
@@ -0,0 +1,13 @@
+package shailesh.org.shubhada;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
